02b6cc68517774eb4f113a7012c5bf57fc26e6ff
satvjain_deloitte <satvjain@deloitte.com>
2025-12-06 19:06:52 +0530

    chore: Initial backend setup with Spring Boot

---
+++ src/main/java/com/kirafx/kirabackend/KirabackendApplication.java
@@ -0,0 +1,13 @@
+package com.kirafx.kirabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KirabackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KirabackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kirafx/kirabackend/KirabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirafx.kirabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KirabackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
